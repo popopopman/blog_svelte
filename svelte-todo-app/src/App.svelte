ac040f8a9640bf7d571261b27d951cf0a62d65b0
tomoki hirano <hirano.tomoki@nifty.co.jp>
2023-11-08 17:24:00 +0900

    second

---
+++ svelte-todo-app/src/App.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	let title = '';
+	let todoList: string[] = ['やること１', 'やること２', 'やること３'];
+  
+	// 作成ボタンを押したときの処理
+	const handleClickCreateButton = () => {
+	  alert(title);
+	  title = '';
+	};
+	</script>
+	
+	<div>
+		<form>
+		<label>
+			タイトル
+			<input bind:value={title} />
+		</label>
+		<button on:click={handleClickCreateButton}>作成</button>
+		</form>
+	</div>
+  
+	{#if todoList.length === 0}
+	<div>アイテムを作成してください</div>
+	{:else}
+	<ul>
+		{#each todoList as todoItem}
+		<li>{todoItem}</li>
+		{/each}
+	</ul>
+	{/if}
