bb30e3f994d37814dc945fe47278e00b6de04845
popopopman <rikepoprike@gmail.com>
2023-11-28 16:50:11 +0000

    CSSちょっといじった

--- svelte-todo-app/src/App.svelte
+++ svelte-todo-app/src/App.svelte
@@ -1,61 +1,55 @@
-<script lang="ts">
-    let title = '';
-    let todoList = [];
+<script lang="ts" type="module" src="./index.js">
+	let title = '';
+	let todoList = [];
 
-    // APIのベースURL（サーバーのURL）
-    const API_BASE_URL = 'http://localhost:3000';
+	// 作成ボタンを押したときの処理
+	const handleClickCreateButton = () => {
+		if (title.trim() !== '') {
+			todoList = [...todoList, title];
+			title = '';
+		}
+	};
 
-    // サーバーからTodoリストを読み込む関数
-    async function loadTodoList() {
-        const response = await fetch(`${API_BASE_URL}/todolist`);
-        todoList = await response.json();
-    }
+	// 削除ボタンを押したときの処理
+	const handleClickDeleteButton = (index) => {
+		todoList = todoList.filter((_, i) => i !== index);
+	};
 
-    // コンポーネントの初期化時にTodoリストを読み込み
-    loadTodoList();
-
-    // サーバーにTodoリストを保存する関数
-    async function saveTodoList() {
-        await fetch(`${API_BASE_URL}/todolist`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(todoList)
-        });
-    }
-
-    // 作成ボタンを押したときの処理
-    function handleClickCreateButton() {
-        if (title.trim() !== '') {
-            const newTodo = { id: Date.now(), title: title };
-            todoList = [...todoList, newTodo];
-            title = '';
-            saveTodoList();
-        }
-    }
-
-    // 削除ボタンを押したときの処理
-    function handleClickDeleteButton(id) {
-        todoList = todoList.filter(todo => todo.id !== id);
-        saveTodoList();
-    }
 </script>
+<style>
+    .mdc-button {
+        background-color: var(--primary-color);
+        color: var(--secondary-color);
+    }
 
+    .mdc-text-field--filled .mdc-floating-label {
+        color: var(--primary-color);
+    }
+
+    .mdc-text-field--filled .mdc-line-ripple {
+        background-color: var(--primary-color);
+    }
+</style>
+	
 <div>
-    <label>
-        タイトル
-        <input bind:value={title} />
+    <label class="mdc-text-field mdc-text-field--filled">
+        <span class="mdc-text-field__ripple"></span>
+        <input class="mdc-text-field__input" type="text" bind:value={title} />
+        <span class="mdc-floating-label">タイトル</span>
+        <span class="mdc-line-ripple"></span>
     </label>
-    <button on:click={handleClickCreateButton}>作成</button>
+    <button class="mdc-button mdc-button--raised" on:click={handleClickCreateButton}>作成</button>
 </div>
 
 {#if todoList.length === 0}
-    <div>アイテムを作成してください</div>
+<div>アイテムを作成してください</div>
 {:else}
-    <ul>
-        {#each todoList as todoItem (todoItem.id)}
-            <li>{todoItem.title} <button on:click={() => handleClickDeleteButton(todoItem.id)}>削除</button></li>
-        {/each}
-    </ul>
+<ul class="mdc-list">
+    {#each todoList as todoItem, index (index)}
+    <li class="mdc-list-item">
+        {todoItem}
+        <button class="mdc-icon-button material-icons" on:click={()=>{handleClickDeleteButton(index)}}>削除</button>
+    </li>
+    {/each}
+</ul>
 {/if}
